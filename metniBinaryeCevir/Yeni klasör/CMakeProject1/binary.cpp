cae06a1cee19908aacb3f10a8e03b02b73923417
DESKTOP-IAVPDO1\Burhan Mutlu <burhackmutlu@gmail.com>
2023-08-01 11:38:23 +0300

    guncelleme

---
+++ metniBinaryeCevir/Yeni klasör/CMakeProject1/binary.cpp
@@ -0,0 +1,22 @@
+﻿#include"binary.hpp"
+
+void binaryGoster(int* binary, std::ofstream& file) {
+    for (int i = 0; i < 8; i++) {
+        file << binary[i];
+    }
+}
+
+int* binaryeCevir(int value) {
+    static int binary[8] = { 0,0,0,0,0,0,0,0 };
+    Bolme b;
+    int i = 0;
+    do {
+        b.bolum = value / 2;
+        b.kalan = value - (b.bolum * 2);
+        binary[7 - i] = b.kalan;
+        value = b.bolum;
+        i++;
+    } while (b.bolum > 0);
+
+    return binary;
+}
---
+++ metniBinaryeCevir/Yeni klasör/CMakeProject1/binary.hpp
@@ -0,0 +1,17 @@
+﻿#ifndef BINARY_HPP
+#define BINARY_HPP
+
+#include<fstream>
+
+struct Bolme {
+    int bolum;
+    int kalan;
+};
+
+//print binary
+void binaryGoster(int* binary, std::ofstream& file);
+
+//decimal to binary
+int* binaryeCevir(int value);
+
+#endif
